86abdbded3aa7a3e1f2c8c64975fb3b190d46e05
Joe Cheng <joe@rstudio.com>
2017-07-13 18:09:37 -0700

    Propagate .visible across pipe-style calls

--- R/pipe.R
+++ R/pipe.R
@@ -46,6 +46,10 @@ magrittr::"%T>%"
   lhs
 }
 
+has.visible <- function(func) {
+  ".visible" %in% names(formals(func))
+}
+
 pipeify_rhs <- function(rhs, env) {
   if (is_parenthesized(rhs)) {
     rhs <- eval(rhs, env, env)
@@ -54,12 +58,26 @@ pipeify_rhs <- function(rhs, env) {
   rhs <- if (is_funexpr(rhs)) {
     rhs
   } else if (is_function(rhs) || is_colexpr(rhs)) {
-    prepare_function(rhs)
+    # This block diverges from magrittr because we have an optional .visible
+    # argument that can be passed to the function. If the function takes a
+    # parameter called .visible then we will pass it, otherwise no.
+
+    real_rhs <- if (is.function(rhs)) {
+      rhs
+    } else {
+      eval(rhs, env, env)
+    }
+
+    if (has.visible(real_rhs)) {
+      as.call(list(rhs, quote(.), .visible = quote(.visible)))
+    } else {
+      as.call(list(rhs, quote(.)))
+    }
   } else if (is_first(rhs)) {
     prepare_first(rhs)
   } else {
     rhs
   }
 
-  eval(call("function", as.pairlist(alist(.=)), rhs), env, env)
+  eval(call("function", as.pairlist(alist(.=, .visible=)), rhs), env, env)
 }
