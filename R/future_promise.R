dd267cb43dc8b2f46b402f1147d64e5ce971e753
Barret Schloerke <schloerke@gmail.com>
2021-01-19 14:15:00 -0500

    namespace `utils::packageVersion()`

--- R/future_promise.R
+++ R/future_promise.R
@@ -2,7 +2,7 @@ assert_future_version <- local({
   val <- NULL
   function() {
     if (!is.null(val)) return()
-    if (packageVersion("future") < "1.21.0") {
+    if (utils::packageVersion("future") < "1.21.0") {
       stop("`future` version >= 1.21.0 is required")
     }
     val <<- TRUE
