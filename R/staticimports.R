8d055a1b8641eab4ab0d05cb69f76452b047034a
Barret Schloerke <schloerke@gmail.com>
2023-02-07 17:08:30 -0500

    Update static imports. Use wch/staticimports#22

--- R/staticimports.R
+++ R/staticimports.R
@@ -3,11 +3,45 @@
 # Imported from pkg:staticimports
 # ======================================================================
 
+get_package_version <- function(pkg) {
+  # `utils::packageVersion()` can be slow, so first try the fast path of
+  # checking if the package is already loaded.
+  ns <- .getNamespace(pkg)
+  if (is.null(ns)) {
+    utils::packageVersion(pkg)
+  } else {
+    as.package_version(ns$.__NAMESPACE__.$spec[["version"]])
+  }
+}
 
-is_installed <- function(package, version = NULL) {
-  installed <- nzchar(system.file(package = package))
+is_installed <- function(pkg, version = NULL) {
+  installed <- isNamespaceLoaded(pkg) || nzchar(system_file_cached(package = pkg))
   if (is.null(version)) {
     return(installed)
   }
-  installed && isTRUE(utils::packageVersion(package) >= version)
+  installed && isTRUE(get_package_version(pkg) >= version)
 }
+
+# A wrapper for `system.file()`, which caches the package file path.
+# Note, only non-empty paths are cached.
+system_file_cached <- local({
+  pkg_dir_cache <- character()
+
+  function(..., package = "base") {
+    if (!is.null(names(list(...)))) {
+      stop("All arguments other than `package` must be unnamed.")
+    }
+
+    not_cached <- is.na(match(package, names(pkg_dir_cache)))
+    if (not_cached) {
+      pkg_dir <- system.file(package = package)
+      if (nzchar(pkg_dir)) {
+        pkg_dir_cache[[package]] <<- pkg_dir
+      }
+    } else {
+      pkg_dir <- pkg_dir_cache[[package]]
+    }
+
+    file.path(pkg_dir, ...)
+  }
+})
