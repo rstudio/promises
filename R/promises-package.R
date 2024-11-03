1e3d1c690b25ff277adc415381bcb6816114b5df
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-11-03 12:23:30 -0500

    refactor to use cpp11 instead of Rcpp

--- R/promises-package.R
+++ R/promises-package.R
@@ -1,9 +1,5 @@
-#' @keywords internal
+#' @useDynLib promises, .registration = TRUE
 "_PACKAGE"
-
-## usethis namespace: start
-## usethis namespace: end
-NULL
 
 ignore_unused_imports <- function() {
   fastmap::fastqueue
--- src/promise_task.cpp
+++ src/promise_task.cpp
@@ -1,55 +1,53 @@
-#include <Rcpp.h>
 #include <later_api.h>
 
-class PromiseTask : public later::BackgroundTask {
-public:
-  PromiseTask(Rcpp::Function resolve, Rcpp::Function reject) :
-    resolve(resolve), reject(reject) {
-  }
+#include <cpp11.hpp>
 
-protected:
+class PromiseTask : public later::BackgroundTask {
+ public:
+  PromiseTask(cpp11::function resolve, cpp11::function reject)
+      : resolve(resolve), reject(reject) {}
+
+ protected:
   virtual void execute() = 0;
-  virtual Rcpp::RObject get_result() = 0;
+  virtual cpp11::sexp get_result() = 0;
 
   void complete() {
-    Rcpp::RObject result = get_result();
+    cpp11::sexp result = get_result();
     resolve(result);
   }
-private:
-  Rcpp::Function resolve;
-  Rcpp::Function reject;
+
+ private:
+  cpp11::function resolve;
+  cpp11::function reject;
 };
 
 long fib(long x) {
   if (x <= 2) {
     return 1;
   }
-  return fib(x-1) + fib(x-2);
+  return fib(x - 1) + fib(x - 2);
 }
 
 class FibonacciTask : public PromiseTask {
-public:
-  FibonacciTask(Rcpp::Function resolve, Rcpp::Function reject, double x) :
-    PromiseTask(resolve, reject), x(x) {
-  }
+ public:
+  FibonacciTask(cpp11::function resolve, cpp11::function reject, double x)
+      : PromiseTask(resolve, reject), x(x) {}
 
-  void execute() {
-    result = fib((long)x);
-  }
+  void execute() { result = fib((long)x); }
 
-  Rcpp::RObject get_result() {
-    Rcpp::NumericVector res(1);
+  cpp11::sexp get_result() {
+    cpp11::writable::doubles res(1);
     res[0] = (double)result;
     return res;
   }
 
-private:
+ private:
   double x;
   long result;
 };
 
-// [[Rcpp::export]]
-void asyncFib(Rcpp::Function resolve, Rcpp::Function reject, double x) {
+[[cpp11::register]] void asyncFib(cpp11::function resolve,
+  cpp11::function reject, double x) {
   FibonacciTask* fib = new FibonacciTask(resolve, reject, x);
   fib->begin();
 }
@@ -57,10 +55,9 @@ void asyncFib(Rcpp::Function resolve, Rcpp::Function reject, double x) {
 /*** R
 library(promises)
 library(later)
-library(Rcpp)
+library(cpp11)
 
 promise(function(resolve, reject) {
-  promise:::asyncFib(resolve, reject, 45)
+  cpp11::package("your_package_name")$asyncFib(resolve, reject, 45)
 }) %...>% print()
-
- */
+*/
