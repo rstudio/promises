d4397c0f99401ce432eca3d1014b71d13f495709
Barret Schloerke <barret@posit.co>
2025-07-24 18:11:45 -0400

    feat(deps): Add otel / otelsdk remote dependencies
    - Add `otel` to imports and `otelsdk` to suggests
    - Configure remote repositories for otel packages
    - Add otel tracer name constant for package identification
    - Include otelsdk removal in release notes
    - Use tidy description

--- R/promises-package.R
+++ R/promises-package.R
@@ -13,6 +13,7 @@ ignore_unused_imports <- function() {
 
 release_bullets <- function() {
   c(
+    "Remove `otelsdk` dependency",
     "Update staticimports: `staticimports::import()`",
     "Revert `future_promise()` example to use `\\donttest{}`, not `\\dontrun{}`",
     "Revert `skip_on_os(\"mac\")` in `future_promise()` test"
