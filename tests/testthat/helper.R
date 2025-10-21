7a981729efeb1591ed2e8c6529a786da6b8a28f5
Barret Schloerke <barret@posit.co>
2025-10-21 12:00:10 -0400

    namespace helper later methods

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -27,11 +27,11 @@ wait_for_it <- function(p = NULL, timeout = if (on_ci) 60 else 30) {
     p |> catch(\(reason) err <<- reason)
   }
 
-  while (!loop_empty()) {
+  while (!later::loop_empty()) {
     if (difftime(Sys.time(), start, units = "secs") > timeout) {
       stop("Waited too long")
     }
-    run_now()
+    later::run_now()
     Sys.sleep(0.01)
   }
 
